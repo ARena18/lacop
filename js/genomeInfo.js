a1c2dd8572c7857460fbefaa8b49cbf2389493a7
ARena18 <91445893+ARena18@users.noreply.github.com>
2024-07-31 09:54:15 -0700

    Update and rename GenomeInfo.js to genomeInfo.js

--- js/genomeInfo.js
+++ js/genomeInfo.js
@@ -1,28 +1,28 @@
 // Author : Richard Charczenko
 // Edited by : Gina Philipose, Rena Ahn, Zachary Mullen
-// JavaScript File : genomeInfo.js
-//   Original File : Python
-//   Translated By : Gina Philipose
+/* JavaScript File : genomeInfo.js
+     The original File was a Python file (GenomeInfo.py) hosted with Flask
+     Translation By : Gina Philipose, Zachary Mullen
+*/
 // Version 2
-// Last Update : July 24th, 2024
+// Last Update : July 30th, 2024
 
 // Purpse : Define the Genome class
 
 class Genome {
-    
     // Constructor
     // Pre : PARAM mutations is a dictionary
     // Post : FIELD mut is set to PARAM mutations;
-    //        FILED counter is set to '0'
-    constructor(mutations){
+    //        FIELD counter is set to '0'
+    constructor(mutations) {
         this.mut = mutations;
-        this.counter = 0;   // !!!!! suspect is not necessary if not implemented FIELD visited
+        this.counter = 0;   // !!! Note : suspect is not necessary if not implemented FIELD visited
     }
 
     // Returns true if PARAM item is in FIELD mut, returns false otherwise
-    // Pre : PARAM item is a string variable (recommended)
+    // Pre : PARAM item is a string variable (recommended for proper output)
     // Post : none
-    has(item) {   // !!!!! Originally a try-catch statement, was the change desired
+    has(item) {   // !!! Note : originally try-catch statement
         if(item in this.mut) {
             return true;
         } 
@@ -49,7 +49,7 @@ class Genome {
     // Returns 'true' if no mutation, returns 'false' otherwise
     // Pre : none
     // Post : none
-    promoter(){
+    promoter() {
         return this.mut["ProMutation"] === null;
     }
     /* Future notes:
@@ -66,10 +66,10 @@ class Genome {
     // Utilizes Function(s)...Repressor.bound
     // Pre : PARAM allo is a number variable,
     //       PARAM lacOut is a number variable,
-    //       PARAM rep is a list of repressor variable,
+    //       PARAM rep is a list of repressor variables,
     //       PARAM glucose is a number variable
     // Post : none
-    operator(allo, lacOut, rep, glucose){
+    operator(allo, lacOut, rep, glucose) {
         if(this.mut["OpMutation"] === null) {
             for(let r of rep) {
                 if(!(r.bound(allo, lacOut, glucose))) {
@@ -80,7 +80,13 @@ class Genome {
         return true;
     }
 
-    // !!!!! FIELD visited, __iter__, next, and __str__ is not implemented, is this desired
+    /* Note on Testing Variables and Functions
+       - Related : FIELD visited, FIELD counter, FUNC __iter__, FUNC next, FUNC __str__
+       - (1) : Excluding FIELD counter, the related fields and functions are not
+               implemented. Is this desired?
+       - (2) : FIELD counter seems related to visited and is not used elsewhere.
+               Is the field then necessary?
+    */
 }
 
 export default Genome;

af069e5753b75f4303aafd833693b16c02ca7c68
ARena18 <91445893+ARena18@users.noreply.github.com>
2024-08-14 03:23:38 -0700

    Update repressor.js

--- js/repressor.js
+++ js/repressor.js
@@ -1,10 +1,10 @@
-// Author : 
+// Author : Richard Charczenko
 // Edited By : Gina Philipose, Rena Ahn, Zachary Mullen
-/* JavaScript File : genomeInfo.js
-     The original File was a Python file (GenomeInfo.py) hosted with Flask
+/* JavaScript File : repressor.js
+     The original File was a Python file (repressor.py) hosted with Flask
      Translation By : Gina Philipose, Zachary Mullen
 */
-// Last Updated : July 30th, 2024
+// Last Updated : August 7th, 2024
 
 // Purpose : Define the Repressor class
 
@@ -13,7 +13,7 @@
 
 class Repressor {
     static repressorMut = {"none": "active", "lacI-": "inactive", "lacIs": "stuck"};
-    // ??? Better as a constant - static variables can be reassigned
+    // ??? Make a constant - static variables can be reassigned
 
     // Constructor
     // Pre : PARAM mut is a string variable
@@ -28,38 +28,29 @@ class Repressor {
     // equal to "active"
     // Utilizes Function(s)...conditionCheck
     // Pre : PARAM allo is a number variable,
-    //       PARAM Le is a number variable ??? why capitalized
-    //       PARAM glu is a number variable
-    bound(allo, Le, glu) {
+    //       PARAM glucose is a number variable
+    bound(allo, glucose) {
         if (this.status == "stuck") {
             return false;
         }
         if (this.status == "active") {
-            return this.conditionCheck(allo, Le, glu);
+            return this.conditionCheck(allo, glucose);
         }
         return true; //if(this.status == "inactive")
     }
 
+    // <Description Missing>
     // Utilizes Function(s)...Math.random
     // Pre : PARAM allo is a number variable,
-    //       PARAM Le is a ___ variable,
-    //       PARAM glu is a number variable
-    conditionCheck(allo, Le, glu){
-        /* Work needs to be done
-           - the [S] vs bound Repressor info is needed to accurately tell if
-             rep is on or off, below is all tentative work
-        */
-        allo = allo - (glu/2);
-        const pBound = (allo / 100.0); // tentative equation
+    //       PARAM glucose is a number variable
+    conditionCheck(allo, glucose){
+        allo = allo - (glucose / 2);
+        const pBound = (allo / 100.0);
+        // Note : pBound was marked to be a tentative equation, [S] vs bound
+        //        Repressor info was requested for further improvement
         const num = Math.random();
-        return (num < pBound); // or Le > 50
+        return (num < pBound);
     }
-
-    /* !!!!! Note on Le
-       - currently the value of lacOut is used in substitute
-       (1) is Le another name for lacOut?
-       (2) if not, what is it? and how is it used?
-    */
 }
 
 export default Repressor;

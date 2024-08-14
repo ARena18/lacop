37de46e8860aca2528bb988db05c45307b5c40da
ARena18 <91445893+ARena18@users.noreply.github.com>
2024-08-14 03:22:22 -0700

    Create graph.js

---
+++ js/graph.js
@@ -0,0 +1,48 @@
+// Author : Rena Ahn
+// JavaScript File : graph.js
+// Last Update : August 13th, 2024
+
+// Purpose : Graph simulation data using chart.js
+
+import graphData from './main.js';
+import { Chart } from 'https://cdn.jsdelivr.net/npm/chart.js@4.4.3/+esm';
+
+let runSimBtn = getElementById("runBtn");
+runSimBtn.addEventListener("click", function() {
+    new Chart(
+        document.getElementById('displayGraph'),
+        {
+            type: 'line',
+            data: {
+            labels: graphData.map(row => row.time),
+            datasets: [
+                {
+                label: 'Allolactose',
+                data: graphData.map(row => row.allo)
+                },
+                {
+                label: 'Beta-galactosidase',
+                data: graphData.map(row => row.bgal)
+                },
+                {
+                label: 'Glucose',
+                data: graphData.map(row => row.glucose)
+                },
+                {
+                label: 'Lactose-In',
+                data: graphData.map(row => row.lacIn)
+                },
+                {
+                label: 'Lactose-Out',
+                data: graphData.map(row => row.lacOut)
+                },
+                {
+                label: 'Permease',
+                data: graphData.map(row => row.perm)
+                },
+            ]
+            }
+        }
+    );
+    console.log("drawGraph ran");
+})

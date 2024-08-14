ec1598c51bbea6d7e6e751d2868fb7b0502e0101
ARena18 <91445893+ARena18@users.noreply.github.com>
2024-08-14 03:37:14 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,48 @@
 //// ask about missing functions?: .includes, .indexOf
 
+import graphData from './main.js';
+import { Chart } from 'https://cdn.jsdelivr.net/npm/chart.js@4.4.3/+esm';
+
+let runSimBtn = getElementById("runBtn");
+runSimBtn.addEventListener("click", function() {   // adding a graph
+    new Chart(
+        document.getElementById('displayGraph'),
+        {
+            type: 'line',
+            data: {
+            labels: graphData.map(row => row.time),
+            datasets: [
+                {
+                label: 'Allolactose',
+                data: graphData.map(row => row.allo)
+                },
+                {
+                label: 'Beta-galactosidase',
+                data: graphData.map(row => row.bgal)
+                },
+                {
+                label: 'Glucose',
+                data: graphData.map(row => row.glucose)
+                },
+                {
+                label: 'Lactose-In',
+                data: graphData.map(row => row.lacIn)
+                },
+                {
+                label: 'Lactose-Out',
+                data: graphData.map(row => row.lacOut)
+                },
+                {
+                label: 'Permease',
+                data: graphData.map(row => row.perm)
+                },
+            ]
+            }
+        }
+    );
+    console.log("drawGraph ran");
+})
+
 
 const display = document.getElementById("displayInputs");
 

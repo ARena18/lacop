4e83107f3efae2cc75896158f04e6f1e9b618987
ARena18 <91445893+ARena18@users.noreply.github.com>
2024-08-14 03:20:45 -0700

    Update bGal.js

--- js/bGal.js
+++ js/bGal.js
@@ -1,14 +1,14 @@
-// Author : 
+// Author : Richard Charczenko
 // Edited By : Gina Philipose, Rena Ahn, Zachary Mullen
-/* JavaScript File : genomeInfo.js
-     The original File was a Python file (GenomeInfo.py) hosted with Flask
+/* JavaScript File : bGal.js
+     The original File was a Python file (bGal.py) hosted with Flask
      Translation By : Gina Philipose, Zachary Mullen
 */
-// Last Update : June 30th, 2024
+// Last Update : August 7th, 2024
 
 // Purpose : Define the Bgal class
 
-// Mimics the functionality of BetaGalactisidase (B-Gal), assuming Mg is a
+// Mimics the functionality of BetaGalactosidase (B-Gal), assuming Mg is a
 // cofactor and reaction takes place at 30 degrees C
 class Bgal {
     // Bgal Constructor
@@ -21,7 +21,7 @@ class Bgal {
     }
 
     // Handles BetaGalactisidase catalyzation
-    // Utilizes function(s)... Lrate(lacIn), Arate(allo), Math.random()
+    // Utilizes function(s)... lacRate, alloRrate, Math.random()
     // Returns changes (JSON object) which stores a number value that differs
     // depending on the values of PARAM lacIn, PARAM allo, and choice (a random
     // value) for the string keys "lac", "allo", and "gluGal"
@@ -42,11 +42,11 @@ class Bgal {
         const p = (lacIn / (allo+lacIn));
         const choice = Math.random();
         if (choice < p && lacIn > 1) {
-            const change = this.Lrate(lacIn);
+            const change = this.lacRate(lacIn);
             changes["lac"] = change * -1;
             changes["allo"] = change;
         } else if(allo > 1){
-            const change = this.Arate(allo);
+            const change = this.alloRate(allo);
             changes["allo"] = change * -1;
             changes["gluGal"] = change;
         }
@@ -58,29 +58,27 @@ class Bgal {
     // Returns (VmaxAllo * allo) / (KmAllo + allo) if FIELD mut = None
     // Pre : PARAM allo is a number variable
     // Post : none
-    Arate(allo) {
+    alloRate(allo) {
         const KmAllo = 9400; // in uM, and found from:http://www.uniprot.org/uniprot/P00722
         const VmaxAllo = 4.97; // in umol/min/mg[E] and found from:http://www.uniprot.org/uniprot/P00722
         if (this.mut === null) {
             const v = (VmaxAllo * allo) / (KmAllo + allo);
             return v;
         }
-        // ??? else return a certain value
     }
 
     // Uses the Michaelis menten equation and the current concentration of lactose to determine
-    // the rate or the amount of lactose reduced
+    // the rate of the amount of lactose reduced
     // Returns (VmaxLactose * lacIn) / (KmLactose + lacIn) if FIELD mut = None
     // Pre : PARAM lacIn is a number variable
     // Post : none
-    Lrate(lacIn) {
+    lacRate(lacIn) {
         const KmLactose = 1350.0; //Found from:http://www.uniprot.org/uniprot/P00722
         const VmaxLactose = 30.9; //Found from:http://www.uniprot.org/uniprot/P00722
         if (this.mut === null) {
             const v = (VmaxLactose * lacIn) / (KmLactose + lacIn); // Michaelis-menten equation
             return v;
         }
-        // ??? else return a certain value
     }
 }
 
